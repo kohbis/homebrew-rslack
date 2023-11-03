815484171de3d4c31e78c0dba21aeb2739567d13
kohbis <18735471+kohbis@users.noreply.github.com>
2023-11-03 22:17:27 +0900

    Update rslack.rb

--- Formula/rslack.rb
+++ Formula/rslack.rb
@@ -3,16 +3,16 @@
 class Rslack < Formula
   desc "TUI for slack message"
   homepage "https://github.com/kohbis/rslack"
-  version "v0.3.2"
+  version "v0.3.3"
 
   on_macos do
     url "https://github.com/kohbis/rslack/releases/download/#{version}/rslack-macos.tar.gz"
-    sha256 "6dd248ddf9d3647d4b6cb89246a73dcb54b38a326b2a5e3ba758cbd95dbbfd7b"
+    sha256 "4c6d892abd749acc90ff1014697c8bb74da2ab45b0ebead6b2ba7d4d02a965e6"
   end
 
   on_linux do
     url "https://github.com/kohbis/rslack/releases/download/#{version}/rslack-linux.tar.gz"
-    sha256 "926e0dc3760d0b62996d4545861b1f3037a6819601954ddec31d28d150486eba"
+    sha256 "8784916054e2fe8b8b6261e2138335fb5e47d2726e454850ca33780579dda806"
   end
 
   def install
