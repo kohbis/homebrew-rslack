9a74df11fea906948f03b93c53352fa38610ee98
Kohei Sugimoto <18735471+kohbis@users.noreply.github.com>
2025-06-14 14:16:34 +0900

    Update rslack.rb

--- Formula/rslack.rb
+++ Formula/rslack.rb
@@ -7,12 +7,12 @@ class Rslack < Formula
 
   on_macos do
     url "https://github.com/kohbis/rslack/releases/download/#{version}/rslack-macos.tar.gz"
-    sha256 "4f4dd0f83625cde90f7007c03c7db7b2aea2da950c7dfcb35fef8c3ad8a01b51"
+    sha256 "4f4dd0f83625cde90f7007c03c7db7b2aea2da950c7dfcb35fef8c3ad8a01b51" # macos
   end
 
   on_linux do
     url "https://github.com/kohbis/rslack/releases/download/#{version}/rslack-linux.tar.gz"
-    sha256 "cc827d39b8eaac0d3fe21d33927c14ce28f6ba1e12f86f7e830a320e95a34182"
+    sha256 "cc827d39b8eaac0d3fe21d33927c14ce28f6ba1e12f86f7e830a320e95a34182" # linux
   end
 
   def install
